f60fa69631e30e774a01e1df822ddd03a8e0f2d3
Jay9874 <jayprakashsharma225@gmail.com>
2022-02-27 21:03:28 +0530

    first commit

---
+++ reports/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReportCardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reports'
---
+++ reports/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+    return render(request, "reports/student.html")
